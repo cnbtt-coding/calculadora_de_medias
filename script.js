33f2caa35b49a77a594c128596e0a2a93d107c07
Caio Nascimento Battista <caio.caiobattista@outlook.com>
2023-08-03 18:19:41 -0300

    Fix: "Added 'toFixed' property to limit length"

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function atualizaTabela () {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-sticker').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
